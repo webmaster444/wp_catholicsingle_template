d51d664fa336fe6f668b1b6433f6fb3982dc3ccb
webmaster444 <jlmobile710@gmail.com>
2017-09-29 20:42:06 +0300

    blog page layout change

--- page-blog.php
+++ page-blog.php
@@ -2,11 +2,11 @@
 
 <?php
   $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-  query_posts( array( 'posts_per_page' => 10, 'post_status' => 'publish', 'paged' => $paged ) );
+  query_posts( array( 'posts_per_page' => 15, 'post_status' => 'publish', 'paged' => $paged ) );
 ?>
 
 <div class="grid-container spacer-top">
-  <div class="grid-75 tablet-grid-75">
+  <div class="grid-100 tablet-grid-100">
     <h1 class="entry-title"><?php 
     if ( is_day() ) { printf( __( 'Daily Archives: %s', 'catholicsingles' ), get_the_time( get_option( 'date_format' ) ) ); }
     elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'catholicsingles' ), get_the_time( 'F Y' ) ); }
@@ -14,14 +14,10 @@
     else { _e( 'CatholicSingles.com Blogs', 'catholicsingles' ); }
     ?></h1>
   </div>
-  <div class="grid-25 tablet-grid-25">
-    <div class="spacer-bottom">
-      <a href="https://app.catholicsingles.com/subscribe.html" class="btn alt message">Subscribe to Newsletter</a>
-    </div>
-  </div>
-  <div class="grid-75 tablet-grid-75 alternate-row-colors">
+
+  <div class="grid-100 tablet-grid-100">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <div class="post-preview grid-container row">
+      <div class="post-preview grid-33">
         <div class="grid-100">
           <?php if (has_post_thumbnail()){ ?>
             <div class="featured-image-block" style="background-image: url(<?php the_post_thumbnail_url('large') ?>);">
@@ -31,17 +27,14 @@
             <h1 class="entry-title"><?php the_title(); ?></h1>
           <?php } ?>
         </div>
+        <span class="bio-photo" style="margin-right:10px;"><?php echo get_the_date('F j, Y'); ?> </span>
+        <span class="cblack">by <?php echo get_author_name(); ?></span>
         <?php the_excerpt(); ?>
         <p class="read-more"><a href="<?php the_permalink(); ?>">Read More</a></p>
       </div>
     <?php endwhile; endif; ?>
     <div class="nav-previous alignleft"><?php previous_posts_link( '&laquo; Newer posts' ); ?></div>
     <div class="nav-next alignright"><?php next_posts_link( 'Older posts &raquo;' ); ?></div>
-  </div>
-  <div class="grid-25 tablet-grid-25">
-    <div id="#blog-index-widget-area" class="blog-sidebar">
-      <?php dynamic_sidebar( 'blog-index-widget-area' ); ?>
-     </div>
   </div>
 </div>
 <?php get_sidebar(); ?>
