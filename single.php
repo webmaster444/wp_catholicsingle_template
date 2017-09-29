4a8015b1c367409af8c6702959220d4fcb80ceb3
webmaster444 <jlmobile710@gmail.com>
2017-09-29 19:59:30 +0300

    single.php fixed

--- single.php
+++ single.php
@@ -2,38 +2,36 @@
 <section id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <div class="grid-container spacer-top blog-page">
-    <div class="grid-75 tablet-grid-75 alternate-row-colors">
+    <div class="grid-80 tablet-grid-80 prefix-10 alternate-row-colors">
       <div class="grid-container">
         <div class="grid-100">
           <div class="breadcrumbs">
-            <a href="/">Home</a> &raquo; <a href="/blog/">Blog</a> &raquo; <?php the_title(); ?>
+            <a href="/">Home</a> &raquo; <a href="/blog/">Blog</a> &raquo; <?php the_title(); ?>          
           </div>
-          <?php if (has_post_thumbnail()){ ?>
-            <div class="featured-image-block" style="background-image: url(<?php the_post_thumbnail_url('large') ?>);">
-              <h1><?php the_title(); ?></h1>
-            </div>
-            <hr />
-          <?php } else { ?>
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-          <?php } ?>
+          <div class="post-categories"><?php echo get_the_category_list();?></div>
+          <br/>
+          <h1 class="h1title"><?php the_title(); ?></h1>
+
+          <span class="bio-photo" style="margin-right:10px;"><?php the_date(); ?> </span>
+          <span class="bio-photo" style="margin-right:10px;"><?php echo get_avatar( get_the_author_id()); ?> </span>
+          <span class="cblack">by <?php echo get_author_name(); ?></span>
+          <br/>
+          <br/>
+          <?php if (has_post_thumbnail()){ ?>            
+            <img src="<?php the_post_thumbnail_url('large');?>" width="100%" />            
+          <?php }  ?>
+                      
           <div class="blog-content">
             <?php the_content(); ?>
           </div>
+          <div class="post-categories"><?php echo get_the_category_list();?></div>
+          <div class="clearfix"></div>
+          <div class="author-section"><span class="bio-photo"><?php echo get_avatar( get_the_author_id()); ?> </span>
+          <span class="bio-text"><?php echo author_excerpt(get_the_author_description()); ?></span></div>
         </div>
 
         <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
       </div>
-    </div>
-    <div class="grid-25 blog-sidebar">
-      <div class="spacer-bottom">
-      <a href="https://app.catholicsingles.com/subscribe.html" class="btn alt message">Subscribe to Newsletter</a>
-      </div>
-
-      <h2>About the Author</h2>
-      <h4><?php the_author(); ?></h4>
-      <span class="bio-photo"><?php echo get_avatar( get_the_author_id()); ?> </span>
-      <?php echo get_the_author_description(); ?>
-      <?php dynamic_sidebar( 'blog-index-widget-area' ); ?>
     </div>
   </div>
 <?php endwhile; endif; ?>
