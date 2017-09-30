bc49ca40d3be9313bf9fd588df950161cca7c165
webmaster444 <jlmobile710@gmail.com>
2017-09-30 22:37:30 +0300

    blog post template added

---
+++ includes/home_recent_blog_posts.php
@@ -0,0 +1,24 @@
+<div class="blog-section-wrapper">
+  <div class="grid-container">
+    <h1 class="grid-100">Catholic Singles Dating Blogs</h1>
+    <?php
+    $blog_query = new WP_Query(array('post_type' => 'post', 'posts_per_page' => 2));
+    $counter = 0;
+    while($blog_query->have_posts()) : $blog_query->the_post();
+      $counter++;
+      ?>
+      <div class="grid-50 post">
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+        <div class="byline">by <span class="author"><?php the_author(); ?></span> on <span class="date"><?php the_date() ?></span></div>
+
+        <div class="post">
+          <p><?php print the_excerpt(); ?>
+        </div>
+
+        <p><a href="<?php the_permalink(); ?>" class="read-more">Read More</a></p>
+      </div>
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); // reset the query ?>
+  </div>
+</div>
