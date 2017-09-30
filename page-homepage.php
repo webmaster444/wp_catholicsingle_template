78171ca98aa4482df53bac459f68bb5e8b770c1a
webmaster444 <jlmobile710@gmail.com>
2017-09-30 21:55:37 +0300

    add homepage template

---
+++ page-homepage.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+<?php get_header(); ?>
+<section id="homepage" role="main">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="grid-container">
+  <div class="grid-100">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php if (false): ?>
+      <header class="header">
+      <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+      </header>
+    <?php endif ?>
+    <section class="entry-content">
+    <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+    <?php the_content(); ?>
+    <div class="entry-links"><?php wp_link_pages(); ?></div>
+    </section>
+    </article>
+    <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+    <?php endwhile; endif; ?>
+    </section>
+  </div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
