e4cce519cf3545167f8222538feb8891c3c01035
webmaster444 <jlmobile710@gmail.com>
2017-10-01 20:00:20 +0300

    last relationback template changed

--- includes/home_last_relation.php
+++ includes/home_last_relation.php
@@ -1,6 +1,6 @@
 <!-- Begin image background with button -->
-<div class="form-section-wrapper">
-  <div class="imgBack" style="background:url('<?php echo get_template_directory_uri();?>/images/lastrelationship.jpg') no-repeat;">
+<div class="img-back-wrapper">
+  <div class="imgBack" style="background:url('<?php echo get_template_directory_uri();?>/images/lastrelationback.jpg') no-repeat;">
     <h1> Our members find long lasting relationships </h1>
     <a href="#" class="btn btn-filled"> Start your free account now! </a>
   </div>  
