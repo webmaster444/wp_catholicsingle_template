3c906641ecbd35dcce3e27cc2521209afad2b534
webmaster444 <jlmobile710@gmail.com>
2017-10-01 20:03:31 +0300

    layout changed

--- includes/home_last_relation.php
+++ includes/home_last_relation.php
@@ -1,8 +1,10 @@
 <!-- Begin image background with button -->
 <div class="img-back-wrapper">
   <div class="imgBack" style="background:url('<?php echo get_template_directory_uri();?>/images/lastrelationback.jpg') no-repeat;">
-    <h1> Our members find long lasting relationships </h1>
-    <a href="#" class="btn btn-filled"> Start your free account now! </a>
+    <div class="widget_content">
+      <h1> Our members find long lasting relationships </h1>
+      <a href="#" class="btn btn-filled"> Start your free account now! </a>
+    </div>
   </div>  
 </div>
 <!-- End image background with button -->
