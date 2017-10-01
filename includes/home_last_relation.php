aa7264dd143b980c7e892015ea60c4fc8432970f
webmaster444 <jlmobile710@gmail.com>
2017-10-01 19:53:09 +0300

    last relation added

--- includes/home_last_relation.php
+++ includes/home_last_relation.php
@@ -1,10 +1,8 @@
 <!-- Begin image background with button -->
 <div class="form-section-wrapper">
-  <div class="grid-container">
-    <div class="imgBack" style="background:url('<?php echo get_template_directory_uri();?>/images/lastrelationship.jpg') no-repeat;">
+  <div class="imgBack" style="background:url('<?php echo get_template_directory_uri();?>/images/lastrelationship.jpg') no-repeat;">
     <h1> Our members find long lasting relationships </h1>
     <a href="#" class="btn btn-filled"> Start your free account now! </a>
-    </div>
-  </div>
+  </div>  
 </div>
 <!-- End image background with button -->
