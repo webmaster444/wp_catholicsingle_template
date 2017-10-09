921bdac6f3b77db62b80ef9f06f0e33a391a3fed
webmaster444 <jlmobile710@gmail.com>
2017-10-09 20:11:43 +0300

    function php edited

--- functions.php
+++ functions.php
@@ -203,34 +203,41 @@ add_filter( 'excerpt_more', 'custom_excerpt_more' );
 
 function theme_option_page() {
 ?>
-<div class="wrap">
-<h1>Custom Theme Options</h1>
-<form method="post" action="options.php">
-<?php
-// display settings field on theme-option page
-settings_fields("theme-options-grp");
-// display all sections for theme-options page
-do_settings_sections("theme-options");
-submit_button();
-?>
-</form>
-</div>
+  <div class="wrap">
+    <h1>Custom Theme Options</h1>
+    
+    <form method="post" action="options.php">
+    <?php
+      // display settings field on theme-option page
+      settings_fields("theme-options-grp");
+      // display all sections for theme-options page
+      do_settings_sections("theme-options");
+      submit_button();
+    ?>
+    </form>
+  </div>
 <?php }
+
 function add_theme_menu_item() {
-add_theme_page("Theme Customization", "Theme Customization", "manage_options", "theme-options", "theme_option_page", null, 99);}
-add_action("admin_menu", "add_theme_menu_item");
-function theme_section_description(){
-echo '<p>Theme Option Section</p>';}
-function options_callback(){
-$options = get_option( 'first_field_option' );
-echo '<input name="first_field_option" id="first_field_option" type="text" class="code" ' . checked( 1, $options, false ) . ' /> Check for enabling custom help text.';
+  add_theme_page("Theme Customization", "Theme Customization", "manage_options", "theme-options", "theme_option_page", null, 99);
 }
+
+add_action("admin_menu", "add_theme_menu_item");
+
+function theme_section_description(){
+  echo '<p>Theme Option Section</p>';
+}
+function options_callback(){
+  $options = get_option( 'first_field_option' );
+  echo '<input name="first_field_option" id="first_field_option" type="text" class="code" ' . 'value='. get_option('first_field_option') . ' /> ';  
+}
+
 function test_theme_settings(){
-add_option('first_field_option',1);// add theme option to database
-add_settings_section( 'first_section', 'Homepage Options Section',
-'theme_section_description','theme-options');
-add_settings_field('first_field_option','2nd Hero Image','options_callback',
-'theme-options','first_section');//add settings field to the “first_section”
-register_setting( 'theme-options-grp', 'first_field_option');
+  add_option('first_field_option',1);// add theme option to database
+  add_settings_section( 'first_section', 'Homepage Options Section',
+    'theme_section_description','theme-options');
+  add_settings_field('first_field_option','2nd Hero Image','options_callback',
+    'theme-options','first_section');//add settings field to the “first_section”
+  register_setting( 'theme-options-grp', 'first_field_option');
 }
 add_action('admin_init','test_theme_settings');
