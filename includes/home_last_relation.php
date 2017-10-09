2e03bd4b31261c0aedb7834d8f8b3fa34590ccdf
webmaster444 <jlmobile710@gmail.com>
2017-10-09 13:05:08 +0300

    connect option

--- includes/home_last_relation.php
+++ includes/home_last_relation.php
@@ -1,10 +1,10 @@
 <!-- Begin image background with button -->
-<div class="img-back-wrapper">
-  <div class="imgBack" style="background:url('<?php echo get_template_directory_uri();?>/images/lastrelationback.jpg') no-repeat;">
-    <div class="widget_content">
-      <h1> Our members find long lasting relationships </h1>
+<div class="img-back-wrapper fluid_width">
+  <div class="imgBack" style='background:url("<?php echo get_option('first_field_option');?>") no-repeat;'>
+    <div class="imgback_widget_content">
+      <h1> Our members find long<br/><em> lasting relationships</em> </h1>
       <a href="#" class="btn btn-filled"> Start your free account now! </a>
     </div>
   </div>  
 </div>
-<!-- End image background with button -->
+<!-- End image background with button -->        
