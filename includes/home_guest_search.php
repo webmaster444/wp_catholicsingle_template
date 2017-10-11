3e8d61419611d0f29d4860777faae40a62f0be2b
webmaster444 <jlmobile710@gmail.com>
2017-10-11 12:07:50 +0300

    guest search edited

--- includes/home_guest_search.php
+++ includes/home_guest_search.php
@@ -1,19 +1,20 @@
 <!-- Form Section -->
-<div class="form-section-wrapper signup_widget">
+<div class="form-section-wrapper fluid_width" id="signup_widget">
   <div class="grid-container">
     <div class="grid-90 prefix-5 form-box search-box tablet-grid-60 tablet-prefix-40 hide-on-mobile">
       <form method="POST" action="https://app.catholicsingles.com/cgi-bin/guest_search.cgi">
         <h1 class="tag_line cwhite"> Find Your Love & Faith </h1>
         <div class='group align-center'>
-          <h1 class="section_title  text-center">Get Started! Create A Free Account:</h1>
+          <h1 class="widget_title  text-center">Get Started! Create A Free Account:</h1>
         </div>
         <div id="login_btns_section" class="text-center">
-        <a href="#" class="btn btn-fb-img btn-blue"><img src="<?php echo get_template_directory_uri();?>/images/fb256.png" alt="Login with Facebook" width='20px'>Login with Facebook</a>
+        <a href="#" class="btn btn-blue" id="fb_login_btn"><img src="<?php echo get_template_directory_uri();?>/images/fb256.png" alt="Login with Facebook" width='20px'>Login with Facebook</a>
         <a href="#" class="btn btn-google-img btn-white" > <img src="<?php echo get_template_directory_uri();?>/images/google256.png" alt="Login with Facebook" width='20px'> Login with Google</a>
         </div>
-        <h2><span>Or signup via email</span></h2>
+        <h2 class="split"><span>Or signup via email</span></h2>
+        <div class="mail-signup-wrapper">
         <div class="group align-center">
-          <div class="grid-33 table-grid-33">
+          <div class="grid-33 table-grid-33" style="text-align:left">
             <input type='email' name='email' placeholder="Email" />
           </div>
           <div class="grid-33 table-grid-33">
@@ -22,9 +23,10 @@
           <div class="grid-33 table-grid-33">
             <input type="text" name="zip" class="zip" placeholder="Zip Code">
           </div>
+          <div class="clearfix"></div>
         </div>
-        <div class='group align-center'>
-          <div class="grid-40 table-grid-40" id="birthday_wrapper">
+        <div class='group align-center' style="margin-top:30px;">
+          <div class="grid-40 table-grid-40 text-left" id="birthday_wrapper">
           <label> Birthday </label>
           <div class="dropdown" id="month_dropdown">
             <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
@@ -70,21 +72,18 @@
                 <li> <a href="#"><?php print $number ?></a></li>
               <?php endforeach; ?>
             </ul>
-          </div>
-            <!-- <select name="">
-              <?php foreach(range(18, 99) as $number): ?>
-                <option value="<?php print $number ?>"><?php print $number ?></option>
-              <?php endforeach; ?>
-            </select> -->
+          </div>            
           </div>
           
           <div class="grid-30 table-grid-30">
-          <input type="text" name="username" placeholder="Username" />
+          <input type="text" name="username" placeholder="Username" style="height:38px"/>
           </div>
 
-          <div class="grid-30 table-grid-30">
+          <div class="grid-30 table-grid-30 mail-signup-btn">
           <a href="#next" class="btn btn-filled">Next</a>
           </div>
+          <div class="clearfix"> </div>
+        </div>
         </div>
       </form>
     </div>
